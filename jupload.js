e54dd3fc7474f94e1234ba52f6e8d6fbf48a2e45
Evyn.Xu <evynxu311@gmail.com>
2015-11-24 09:19:08 +0800

    Remove the jquery dependencies
    Remove the jquery dependencies

--- jupload.js
+++ jupload.js
@@ -2,46 +2,47 @@
 	var that;
 	var model = function(el, options) {
 		that = this;
-
-		that.$el = $(el);
-		that.crlf = '\r\n';
-		that.boundary = "emptychar0";
-		that.dashes = "--";
-
+		that.$el = document.querySelector(el);
 		that.settings = {
 			"maxSize": "",
 			"name": "uploadFileName",
 
-			/* 
+			/**
 			 * 文件筛选
 			 * audio/* 可以接受所有的音频文件
 			 * video/*	可以接受所有的视频文件
 			 * image/*	可以接受所有的图片文件
 			*/
 			"accept": "",
-			/*
-			 * 上传服务端URL地址
-			*/
-			"posturl": "",
-
-			//本地加载相关事件  必须支持window.FileReader
+			"postUrl": "",
 			"onClientLoad": null,
-			//服务端上传监听事件
 			"onServerProgress": null,
 			"onServerReadyStateChange": null,
 
-			"onError": function(res) {alert("file upload error code=" + res.code + " message=" + res.message);},
-			//成功回调函数
-			"onSuccess": function(){console.log('upload success')},
-			//点击事件前验证 return true | false
-			"onBeforeClick": function(){console.log('upload onBeforeClick default'); return true;} 
+			"onError": function(res) {
+                alert("file upload error code=" + res.code + " message=" + res.message);
+            },
+            /**
+             * 成功回调函数
+             */
+			"onSuccess": function(){
+                console.log('upload success');
+            },
+            /**
+             * 点击事件前验证
+             * @returns true | false
+             */
+			"onBeforeClick": function(){
+                console.log('upload onBeforeClick default');
+                return true;
+            }
 		};
 
-		for (key in options) {
+		for (var key in options) {
 			that.settings[key] = options[key];
 		}
 
-		that.$el.css('position', 'relative');
+		that.$el.style.cssText = "position:relative;";
 
 		var input = document.createElement("input");
 		input.id = "__upload_input__";
@@ -49,9 +50,9 @@
 		input.accept = that.settings.accept;
 		input.style.cssText = "position:absolute;left:-10000px;top:-10000px;";
 
-		that.$el.append(input);
+		that.$el.appendChild(input);
 
-		that.$el.bind("click", function() {
+		that.$el.onclick = function() {
 			if (that.settings.onBeforeClick) {
 				if (that.settings.onBeforeClick()) {
 					input.click();
@@ -59,16 +60,14 @@
 			} else {
 				input.click();
 			}
-
-		})
-		$(input).bind("change", function() {
+		};
+		input.onchange =  function() {
 			var files = this.files;
 			for (var i = 0; i < files.length; i++) {
 				that.fileHandler(files[i]);
 			}
-		});
-
-	}
+		};
+	};
 
 	model.prototype = {
 		fileHandler: function(file) {
@@ -96,22 +95,21 @@
 				}
 			}
 			var xmlHttpRequest = new XMLHttpRequest();
-			if ('serverevent' == 'serverevent') {
-				xmlHttpRequest.upload.onprogress = function(e) {
-					if (that.settings.onServerProgress) {
-						that.settings.onServerProgress(e, file);
-					}
-				};
-				xmlHttpRequest.onreadystatechange = function(e) {
-					if (that.settings.onServerReadyStateChange) {
-						that.settings.onServerReadyStateChange(e, file, xmlHttpRequest.readyState);
-					}
-					if (that.settings.onSuccess && xmlHttpRequest.readyState == 4 &&
-						xmlHttpRequest.status == 200) {
-						that.settings.onSuccess(e, file, xmlHttpRequest.responseText);
-					}
-				};
-			}
+
+            xmlHttpRequest.upload.onprogress = function(e) {
+                if (that.settings.onServerProgress) {
+                    that.settings.onServerProgress(e, file);
+                }
+            };
+            xmlHttpRequest.onreadystatechange = function(e) {
+                if (that.settings.onServerReadyStateChange) {
+                    that.settings.onServerReadyStateChange(e, file, xmlHttpRequest.readyState);
+                }
+                if (that.settings.onSuccess && xmlHttpRequest.readyState == 4 &&
+                    xmlHttpRequest.status == 200) {
+                    that.settings.onSuccess(e, file, xmlHttpRequest.responseText);
+                }
+            };
 
 			var url = that.settings.postUrl;
 			if (typeof that.settings.postUrl === "function")
@@ -122,7 +120,7 @@
 			formData.append(that.settings.name, file);
 			xmlHttpRequest.send(formData);
 		}
-	}
+	};
 	window.JUpload = model;
 
-})()
+})();
