8e956043e70566c64d7c6ca277378882a18ee017
Evyn.Xu <evynxu311@gmail.com>
2015-12-01 18:02:28 +0800

    test
    test

--- jupload.js
+++ jupload.js
@@ -1,5 +1,6 @@
 (function() {
 	var model = function(el, options) {
+		//测试分支合并 冲突解决
 		var that = this;
 		that.$el = document.querySelector(el);
 		that.settings = {
@@ -11,7 +12,7 @@
 			 * audio/* 可以接受所有的音频文件
 			 * video/*	可以接受所有的视频文件
 			 * image/*	可以接受所有的图片文件
-			*/
+			 */
 			"accept": "",
 			"postUrl": "",
 			"onClientLoad": null,
@@ -19,22 +20,22 @@
 			"onServerReadyStateChange": null,
 
 			"onError": function(res) {
-                alert("file upload error code=" + res.code + " message=" + res.message);
-            },
-            /**
-             * 成功回调函数
-             */
-			"onSuccess": function(){
-                console.log('upload success');
-            },
-            /**
-             * 点击事件前验证
-             * @returns true | false
-             */
-			"onBeforeClick": function(){
-                console.log('upload onBeforeClick default');
-                return true;
-            }
+				alert("file upload error code=" + res.code + " message=" + res.message);
+			},
+			/**
+			 * 成功回调函数
+			 */
+			"onSuccess": function() {
+				console.log('upload success');
+			},
+			/**
+			 * 点击事件前验证
+			 * @returns true | false
+			 */
+			"onBeforeClick": function() {
+				console.log('upload onBeforeClick default');
+				return true;
+			}
 		};
 
 		for (var key in options) {
@@ -60,7 +61,7 @@
 				input.click();
 			}
 		};
-		input.onchange =  function() {
+		input.onchange = function() {
 			var files = this.files;
 			for (var i = 0; i < files.length; i++) {
 				that.fileHandler(files[i]);
@@ -96,20 +97,20 @@
 			}
 			var xmlHttpRequest = new XMLHttpRequest();
 
-            xmlHttpRequest.upload.onprogress = function(e) {
-                if (that.settings.onServerProgress) {
-                    that.settings.onServerProgress(e, file);
-                }
-            };
-            xmlHttpRequest.onreadystatechange = function(e) {
-                if (that.settings.onServerReadyStateChange) {
-                    that.settings.onServerReadyStateChange(e, file, xmlHttpRequest.readyState);
-                }
-                if (that.settings.onSuccess && xmlHttpRequest.readyState == 4 &&
-                    xmlHttpRequest.status == 200) {
-                    that.settings.onSuccess(e, file, xmlHttpRequest.responseText);
-                }
-            };
+			xmlHttpRequest.upload.onprogress = function(e) {
+				if (that.settings.onServerProgress) {
+					that.settings.onServerProgress(e, file);
+				}
+			};
+			xmlHttpRequest.onreadystatechange = function(e) {
+				if (that.settings.onServerReadyStateChange) {
+					that.settings.onServerReadyStateChange(e, file, xmlHttpRequest.readyState);
+				}
+				if (that.settings.onSuccess && xmlHttpRequest.readyState == 4 &&
+					xmlHttpRequest.status == 200) {
+					that.settings.onSuccess(e, file, xmlHttpRequest.responseText);
+				}
+			};
 
 			var url = that.settings.postUrl;
 			if (typeof that.settings.postUrl === "function")
